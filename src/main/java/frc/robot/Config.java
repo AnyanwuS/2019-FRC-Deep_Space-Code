d43f81558fe7bbd0278071aeb75ea785aab2c081
mvu <kaisenbot@protonmail.com>
2019-02-17 19:38:43 -0600

    Additional constants added

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -2,14 +2,14 @@ package frc.robot;
 
 public class Config {
     //Power
-    protected static final double beltPower = 0.6;
-    protected static final double feederIntakePower = 0.7;
-    protected static final double feederExhaustPower = 0.7;
+    public static final double beltPower = 0.6;
+    public static final double feederIntakePower = 0.7;
+    public static final double feederExhaustPower = 0.7;
     //Limelight
     public static final double Kp = 0.007;
     public static final double mountAngle = 25.0;
 
-    public static final double cameraHeight = 1.168;
+    public static final double cameraHeight = 1.168; //Camera height in meters
     public static final double rcHeight = 0.925; //Rocket cargo height in meters
     public static final double hpHeight = 0.699; //Hatch panel height in meters
     public static final double scHeight = 0.710; //Cargo ship height in meters
